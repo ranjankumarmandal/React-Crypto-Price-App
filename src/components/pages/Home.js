3ff41747ab430a8f16996d43aaf7fadc7a24b915
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-19 12:06:15 +0530

    added componentDidMount() lifecycle method using useEffect() hook

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Home = () => {
   const [listVaue, setListValue] = useState([]);
   const [text, setText] = useState('');
 
-  useEffect(() => {
-    console.log('DOM loaded');
+  useEffect(async () => {
+    let response = await axios.get(
+      'https://api.coinstats.app/public/v1/coins?skip=0'
+    );
+    setListValue(response.data.coins);
   }, []);
 
   return (
     <div>
-      <p>Hello world - home page</p>
+      {listVaue.map((coin) => (
+        <div>{coin.name}</div>
+      ))}
     </div>
   );
 };
