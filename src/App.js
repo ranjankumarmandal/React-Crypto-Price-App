d277eebdadd0af5658c7cde8e5d304587633dd49
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-19 11:33:30 +0530

    started Home and About component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <div className='App'>
-      <div>Hello react crypto app</div>
+      <Home />
     </div>
   );
 }
