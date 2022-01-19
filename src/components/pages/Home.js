9a0628064b533ba695343772ef0f76d28fe00c55
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-19 11:44:07 +0530

    added state and useEffect(for using componentDidMount() lifecycle method

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
+  const [listVaue, setListValue] = useState([]);
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    console.log('DOM loaded');
+  }, []);
+
   return (
     <div>
       <p>Hello world - home page</p>
